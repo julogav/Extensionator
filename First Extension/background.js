80436bb1dd4ecb20b69e746835cab6f64f355c98
Keren <57706914+kerenk100@users.noreply.github.com>
2021-01-30 23:07:16 +0200

    Extensionator: Adding the files for the first extension
    This extension will add an option to the right-click menu that will increase the font size

---
+++ First Extension/background.js
@@ -0,0 +1,13 @@
+function  incFont( info, tab) {
+  chrome.tabs.executeScript({
+    code: `document.body.style.fontSize = "32px"`
+  })
+}
+
+chrome.runtime.onInstalled.addListener ( function () {
+  chrome.contextMenus.create({
+    "title": "Super Font",
+    "contexts": ["page"],
+    "onclick": incFont
+  })
+});
